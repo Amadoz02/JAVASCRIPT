6c5afd04342869da0194de659f78fa810714f28d
Wilson Delgado <wilsondelgadomoreno@gmail.com>
2024-11-29 11:23:53 -0500

    clase 3 explica

--- principios.js
+++ principios.js
@@ -1,1 +1,56 @@
 
+// let numero1 = parseInt(prompt("ingerese numero 1"));
+// let numero2 = parseInt(prompt("ingerese numero 2"));
+
+// function numAmigos(num1, num2) {
+//     let suma1 = 0;
+//     let suma2 = 0;
+//     for(let i=1;i<num1;i++){
+//         if(num1%i === 0) suma1=suma1+i;
+//     }
+
+//     for(let n=1;n<num2;n++){
+//         if(num2%n === 0) suma2=suma2+n;
+//     }
+    
+//     if(suma1===numero2 && suma2 === numero1) {
+        
+//         console.log(numero1 + "  es numero amigo de  " + numero2);
+//     }
+//     if (numero1==suma1){
+//         console.log(numero1 + " este numero es perfecto");
+//     }
+//     if (numero2==suma2){
+//         console.log(numero2 + " este numero es perfecto");
+//     }
+//     else{
+//         return(numero1 + " y " + numero2 + " no son perfectos y  "+ numero1 + "  y  " + numero2 + " no son numeros amigos");
+//     }
+    
+// }
+
+
+// console.log(numAmigos(numero1, numero2));
+// function sumar(a,b, ...c){
+//     let resultado = 0;
+//     for (var i=0; i<c.length; i++) {
+//         if (typeof c[i]=="number") resultado +=c[i];
+//     }
+//     if (typeof a=="number") resultado+=a;
+//     if (typeof b=="number") resultado+=b;
+//     return resultado;
+// }
+
+// console.log(sumar("dos",2,3,4,"uno",21,10));
+
+// class Persona{
+//     constructor(nombre, edad, apellido){
+//         this.nombre = nombre;
+//         this.apellido = apellido;
+//         this.edad = edad;
+//     }
+// }
+// class Estudiante extends Persona{
+    
+
+// }
